37baa4b2c0107b4bdd49935d0dbeb07d4a0d9391
ahmed.yar.zahid <ahmed.yar.zahid@hotmail.com>
2023-11-30 22:29:49 +0500

    Added comments on functions.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,22 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\Category;
 
 class CategoryController extends Controller
 {
+    /**
+     * Categories list view.
+     *
+     * @return Application|Factory|View|\Illuminate\Foundation\Application
+     */
     public function index()
     {
         $categories = Category::all();
         return view('categories.index', compact('categories'));
     }
 
+    /**
+     * Create category.
+     *
+     * @return Application|Factory|View|\Illuminate\Foundation\Application
+     */
     public function create()
     {
         return view('categories.create');
     }
 
+    /**
+     * Save category.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function store(Request $request)
     {
         $request->validate([
@@ -29,11 +49,24 @@ class CategoryController extends Controller
         return redirect()->route('categories.index')->with('success', 'Category created successfully.');
     }
 
+    /**
+     * Category detail view.
+     *
+     * @param Category $category
+     * @return Application|Factory|View|\Illuminate\Foundation\Application
+     */
     public function edit(Category $category)
     {
         return view('categories.edit', compact('category'));
     }
 
+    /**
+     * Update category.
+     *
+     * @param Request $request
+     * @param Category $category
+     * @return RedirectResponse
+     */
     public function update(Request $request, Category $category)
     {
         $request->validate([
@@ -45,6 +78,12 @@ class CategoryController extends Controller
         return redirect()->route('categories.index')->with('success', 'Category updated successfully.');
     }
 
+    /**
+     * Delete category.
+     *
+     * @param Category $category
+     * @return RedirectResponse
+     */
     public function destroy(Category $category)
     {
         $category->delete();
